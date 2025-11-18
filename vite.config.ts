badccb2581906f3bce92e43c9fe9356d08e1c9a6
Ernesto Serrano <erseco@gmail.com>
2025-11-18 19:41:35 +0000

    lutimos arreglos

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Importante para que los assets carguen correctamente en GitHub Pages
+  base: '/despliegue_exelearning/', // Importante para que los assets carguen correctamente en GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
